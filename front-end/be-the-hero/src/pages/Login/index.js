5bbd0c88dbf6c9652fa1a07174282caa58924c3a
Rafael <rafael.amtx@gmail.com>
2020-03-29 16:37:33 -0300

    realizando login com o back-end

--- front-end/be-the-hero/src/pages/Login/index.js
+++ front-end/be-the-hero/src/pages/Login/index.js
@@ -1,21 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 import './styles.css';
+
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
 
 export default function Login() {
+
+    const [id, setId ] = useState('');
+    const history = useHistory();
+
+    async function handleLogin(e){
+        e.preventDefault();
+
+        try {
+            const response = await api.post('session', { id })
+            
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name);
+            history.push('/profile');
+
+        }catch (err) {
+            alert('Error! Tente novamente');
+            console.log(err);
+        }
+    }
+
     return (
         <div className="login-container">
             <section className="form">
                 <img src={logoImg} alt="Heroes"/>
 
-                <form action="">
+                <form onSubmit={handleLogin}>
                     <h1>Faça seu login</h1>
                     
-                    <input placeholder="Sua ID"/>
+                    <input 
+                        placeholder="Sua ID"
+                        value={id}
+                        onChange={e => setId(e.target.value)}    
+                    />
                     <button type="submit" className="button">Entrar</button>
 
                     <Link className="back-link" to="/register">
